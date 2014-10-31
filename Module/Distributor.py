d4d96aace597529a10f16985d6eb5fbbcbc49659
ChristianF88 <chrisfoe23@aol.de>
2014-10-31 15:48:25 +1100

    Distributor
    It's running now! Even managed to simplifie the code a bit.

--- Module/Distributor.py
+++ Module/Distributor.py
@@ -33,58 +33,28 @@ class Distributor(pycd3.Node):
     def __init__(self):
         pycd3.Node.__init__(self)
         self.inflow = pycd3.Flow()
-        self.Out1 = pycd3.Flow()
-        self.Out2 = pycd3.Flow()
-        self.Out3 = pycd3.Flow()
-        self.Out4 = pycd3.Flow()
-        self.Out5 = pycd3.Flow()
-        
-        #self.number_outports = pycd3.Double(3)
-        #self.addParameter("Number of Outports", self.number_outports)
-        
-        #L=zip(['self.addOutPort("Out1", self.Out1)','self.addOutPort("Out2", self.Out2)','self.addOutPort("Out3", self.Out3)','self.addOutPort("Out4", self.Out4)','self.addOutPort("Out5", self.Out5)','self.addOutPort("Out6", self.Out6)','self.addOutPort("Out7", self.Out7)','self.addOutPort("Out8", self.Out8)','self.addOutPort("Out9", self.Out9)','self.addOutPort("Out10", self.Out10)'])
-        
-        #for i in range(self.number_outports):
-        #    exec "%s" % L[i]
-        
-        
-        
-        print "init node"
-        
-        self.addInPort("In", self.inflow)
-        self.addOutPort("Out1", self.Out1)
-        self.addOutPort("Out2", self.Out2)
-        self.addOutPort("Out3", self.Out3)
-        self.addOutPort("Out4", self.Out4)
-        self.addOutPort("Out5", self.Out5)
-     
-       
-       
-            	
+        self.numberof_out_ports = pycd3.Integer(2)
+        self.addParameter("outports", self.numberof_out_ports)            
         
     def init(self, start, stop, dt):
         print start
         print stop
         print dt
+        print "init node"
+        
+        self.addInPort("In", self.inflow)
+
+        for i in range(self.numberof_out_ports):
+            exec 'self.Out'+str(i)+'=pycd3.Flow()'
+            exec 'self.addOutPort("Out'+str(i)+'", self.Out'+str(i)+')'        
+         
         return True
         
     def f(self, current, dt):
-        
-        
-        
-        self.Out1[0] = self.inflow[0]
-        self.Out2[0] = self.inflow[0]
-        self.Out3[0] = self.inflow[0]
-        self.Out4[0] = self.inflow[0]
-        self.Out5[0] = self.inflow[0]
-        
-        
-        
-        
-        
-      
-       
-
+              
+        for i in range(self.numberof_out_ports):
+            exec 'self.Out'+str(i)+'[0]=self.inflow[0]'
+          
         return dt
     
     def getClassName(self):
